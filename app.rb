5550bcf5fc8dbfe0caa2aaa4e5837487291dd5c3
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-06-14 03:27:58 +0000

    set up erb page pathways

--- app.rb
+++ app.rb
@@ -7,3 +7,47 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/howdy") do 
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:square_calc)
+end
+
+get("/square/results") do
+  @the_num =  params.fetch("users_number").to_f
+  @result = @the_num ** 2
+  erb(:square_result)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @the_num =  params.fetch("users_number").to_f
+  @result = @the_num ** 0.5
+  erb(:root_result)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do 
+  erb(:random_result)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  erb(:payment_results)
+end
